ed234c8ddaf405bddaf315b19d181e9dccce65ef
SnO2WMaN <me@sno2wman.net>
2022-11-10 22:29:57 +0900

    sampledata

--- mod.ts
+++ mod.ts
@@ -1,9 +1,16 @@
 import { Application, Router } from "oak/mod.ts";
+import videos from "./data/videos.json" assert { type: "json" };
 
 const app = new Application();
 const router = new Router();
 
-router.put("/video/:id", async ({ params, request, response }) => {
+router.get("/videos/:id", ({ params, request, response }) => {
+  const video = videos.find(({ id }) => params.id === id);
+  if (!video) {
+    response.status = 400;
+    return;
+  }
+  response.body = video;
 });
 
 app.use(router.routes());
